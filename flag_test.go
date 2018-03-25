911c2206dd17124c29e85a9c647f746db942ce0e
Timo Savola <timo.savola@iki.fi>
2018-03-26 00:15:10 +0300

    test flag error conditions
    fixes #1

--- flag_test.go
+++ flag_test.go
@@ -6,37 +6,79 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
-func TestSetter(t *testing.T) {
+func TestAssigner(t *testing.T) {
 	c := new(testConfig)
 
 	s := flag.NewFlagSet("test", flag.PanicOnError)
 	s.Var(Assigner(c), "c", "path.to.key=value")
 
-	if err := s.Parse([]string{
-		"-c", "foo.key1=true",
-		"-c", "foo.key2=-10",
-		"-c", "foo.key2b=-128",
-		"-c", "foo.key3a=-32768",
-		"-c", "foo.key3=-11",
-		"-c", "foo.key4=-100000000000000",
-		"-c", "foo.key5=10",
-		"-c", "foo.key5b=255",
-		"-c", "foo.key6a=65535",
-		"-c", "foo.key6=11",
-		"-c", "foo.key7=100000000000000",
-		"-c", "foo.key8=1.5",
-		"-c", "foo.key9=1.0000000000005",
-		"-c", "foo.key10=hello, world",
-		"-c", "bar=12345",
-		"-c", "baz.quux.key_a=true",
-		"-c", "baz.quux.key_b=yes",
-		"-c", "baz.interval=10h9m8s7ms6µs5ns",
-	}); err != nil {
-		t.Fatal(err)
+	t.Run("Ok", func(t *testing.T) {
+		if err := s.Parse([]string{
+			"-c", "foo.key1=true",
+			"-c", "foo.key2=-10",
+			"-c", "foo.key2b=-128",
+			"-c", "foo.key3a=-32768",
+			"-c", "foo.key3=-11",
+			"-c", "foo.key4=-100000000000000",
+			"-c", "foo.key5=10",
+			"-c", "foo.key5b=255",
+			"-c", "foo.key6a=65535",
+			"-c", "foo.key6=11",
+			"-c", "foo.key7=100000000000000",
+			"-c", "foo.key8=1.5",
+			"-c", "foo.key9=1.0000000000005",
+			"-c", "foo.key10=hello, world",
+			"-c", "bar=12345",
+			"-c", "baz.quux.key_a=true",
+			"-c", "baz.quux.key_b=yes",
+			"-c", "baz.interval=10h9m8s7ms6µs5ns",
+		}); err != nil {
+			t.Fatal(err)
+		}
+
+		testConfigValues(t, c)
+	})
+
+	s.SetOutput(ioutil.Discard)
+
+	for i := 0; i < 10; i++ {
+		t.Run("WrongType", func(t *testing.T) {
+			defer func() {
+				if recover() == nil {
+					t.Fail()
+				}
+			}()
+			s.Parse([]string{
+				"-c", fmt.Sprintf("foo.key%d=this is a string", i),
+			})
+			t.Fail()
+		})
 	}
 
-	testConfigValues(t, c)
+	for _, path := range []string{
+		"nonexistent",
+		"non.existent",
+		"foo.nonexistent",
+		"nonexistent.key1",
+		"baz.quux.nonexistent",
+		"baz.quux.0",
+		"0",
+	} {
+		t.Run("UnknownPath", func(t *testing.T) {
+			defer func() {
+				if recover() == nil {
+					t.Fail()
+				}
+			}()
+			s.Parse([]string{
+				"-c", fmt.Sprintf("%s=1", path),
+			})
+			t.Fail()
+		})
+	}
 }
